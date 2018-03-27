45636044cafa331b1a4b65dd8b05b64383808694
HanDongheon <han920415@gmail.com>
2018-03-27 12:59:41 +0900

    test1

--- src/algorithm/Main.java
+++ src/algorithm/Main.java
@@ -1,10 +1,16 @@
 package algorithm;
 
+import algorithm.programmers.level3.NoOvertime;
+
 public class Main {
+
+	public static NoOvertime noOvertime = new NoOvertime();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("main");
+		int[] test = { 4, 3, 3 };
+		System.out.println(noOvertime.noOvertime(4, test));
+
 	}
 
 }
